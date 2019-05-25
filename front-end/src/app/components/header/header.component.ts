738f76a1329cd73d5f2e15c3ef80140e0b9ab1de
cesar <cesarleandromayorga7@gmail.com>
2019-05-25 16:51:53 -0500

    implementacion de session y de cierre de session y eliminacion de cache
    implementacion de session y de cierre de session y eliminacion de cache

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Router, ActivatedRoute } from '@angular/router';
-declare var $ : any;
+declare var $: any;
 
 //service auth
 import { AuthService } from '../../services/auth.service';
@@ -16,24 +16,28 @@ export class HeaderComponent implements OnInit {
 
   //rol user
   idSesionRol = '';
+  //id user
+  idSesionUser = '';
   //list data auth menu
   listMenuAuth: {};
 
   constructor(private http: Http, private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
-    $(document).ready(function(){
+    $(document).ready(function () {
       $(".dropdown-trigger").dropdown();
       $('.slider').slider({
-        indicators:false
+        indicators: false
       });
-  });
+    });
     //asign rol user to search data from menu
     this.idSesionRol = localStorage.getItem('idSesionRol');
     //init service validation menu
     this.getUserValidateMenuRol();
+    //validate session
+    this.validateSession();
   }
-  
+
   //obtain data validate menu
   getUserValidateMenuRol() {
     //send to ws api mysql search data for rol user
@@ -42,9 +46,34 @@ export class HeaderComponent implements OnInit {
         if (data != null) {
           //send json response to list
           this.listMenuAuth = data.rows;
-          console.log(this.listMenuAuth);
         }
       });
+  }
+  //validate session user
+  validateSession() {
+    //asign rol user to search data from menu
+    this.idSesionRol = localStorage.getItem('idSesionRol');
+    //asign rol user to search data from menu
+    this.idSesionUser = localStorage.getItem('idSesionUser');
+    if (this.idSesionRol == null || this.idSesionUser == null) {
+      this.router.navigate(['/'])
+    }
+  }
+  //close user session
+  closeSession() {
+    //remove items
+    localStorage.removeItem('idSesionRol');
+    localStorage.removeItem('idSesionUser');
+    localStorage.removeItem('idCategory');
+    localStorage.removeItem('idModule');
+    localStorage.removeItem('idPod');
+    localStorage.removeItem('idPrivilege');
+    localStorage.removeItem('idProduct');
+    localStorage.removeItem('idRol');
+    localStorage.removeItem('idSale');
+    localStorage.removeItem('idTax');
+    //redirect to login
+    this.router.navigate(['/'])
   }
 
 }
