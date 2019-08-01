d14ec30c3cb936af5041240c34157c3b1a116223
Amarelle <ewige.qual@gmail.com>
2019-07-31 19:51:36 -0500

    Auto stash before rebase of "refs/heads/master"

--- back-end/app.js
+++ back-end/app.js
@@ -69,6 +69,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 //urls
 //url init
+app.disable('etag'); 
 app.use('/', indexRouter);
 //url login
 app.post('/auth', login)
